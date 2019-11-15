bcd1cfaf0deecd416092f561c31263da6c451a10
Lautaro Ezequiel Murua <lautaroemurua@gmail.com>
2019-11-15 17:34:20 -0300

    fix login

--- BackEnd/controllers/login.js
+++ BackEnd/controllers/login.js
@@ -1,5 +1,6 @@
-var Schema = require('../models/patient')
+var UserSchema = require('../models/Users')
 var mongoose = require('mongoose')
+const user = require('../models/Users')
 const jwt = require('jsonwebtoken')
 const app = require('express')()
 const config = require('../config/config').config
@@ -7,15 +8,11 @@ const config = require('../config/config').config
 app.set('JWTKey', config.JWTKey);
 var controller = {}
 
-controller.login = (req, res) => {
-  // await Schema.find({ username: req.body.username, password: req.body.password })
-  // .exec((err, data) => {
-  //   if (err) return res.status(500).json(err)
-  //   if (!data) return res.status(404).json( { msg: 'Not found' } )
-  //   return res.send(data)
-  // })
-
-  if (req.body.usuario === "asfo" && req.body.contrasena === "holamundo") {
+controller.login = async (req, res) => {
+  await UserSchema.find({ username: req.body.username, password: req.body.password })
+  .exec((err, data) => {
+    if (err) return res.status(500).json(err)
+    if (!data) return res.status(404).json( { message: 'User or password incorrect' } )
     const payload = {
       check: true
     };
@@ -26,9 +23,23 @@ controller.login = (req, res) => {
       message: 'Autenticación correcta',
       token: token
     });
-  } else {
-    res.json({ mensaje: "Usuario o contraseña incorrectos" })
-  }
+    return res.send(data)
+  })
+
+  // if (req.body.usuario === "asfo" && req.body.contrasena === "holamundo") {
+  //   const payload = {
+  //     check: true
+  //   };
+  //   const token = jwt.sign(payload, app.get('JWTKey'), {
+  //     expiresIn: 1440
+  //   });
+  //   res.json({
+  //     message: 'Autenticación correcta',
+  //     token: token
+  //   });
+  // } else {
+  //   res.json({ mensaje: "Usuario o contraseña incorrectos" })
+  // }
 
 }
 
