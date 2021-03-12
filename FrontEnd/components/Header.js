233272528f853c206e9dc64e3c542d0889968fcb
Lautaro Ezequiel Murua <lautaroemurua@gmail.com>
2021-03-11 22:08:23 -0300

    First Commit

---
+++ FrontEnd/components/Header.js
@@ -0,0 +1,16 @@
+// File - /components/Header.js
+import { Container, Row, Col, Navbar, Nav, NavDropdown } from 'react-bootstrap'
+
+const headerStyle = {};
+  
+const Header = () => (
+<header>
+    <Navbar className="Header" >
+        <Container className="d-flex justify-content-between px-3">
+            <a href="#" className="navbar-brand d-flex align-items-center py-2">DataSurf</a>
+        </Container>
+    </Navbar>
+</header>
+);
+
+export default Header;
